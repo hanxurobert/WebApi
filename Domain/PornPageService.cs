a8fafd90dec2fe1cf33a8928f04a14fb1941f864
Robert Han <hanxurobert@gmail.com>
2020-01-04 22:16:43 -0500

    Update the imgUrl to HD version.

--- Domain/PornPageService.cs
+++ Domain/PornPageService.cs
@@ -38,7 +38,7 @@ namespace PornWebApi.Domain
                 pornPageItem.ViewKey = contentUrl.Substring(contentUrl.IndexOf("=", StringComparison.Ordinal) + 1);
                 var imgNode = aNode.ChildNodes.FindFirst("img");
                 pornPageItem.Title = imgNode.Attributes["title"].Value;
-                pornPageItem.ImgUrl = imgNode.Attributes["src"].Value;
+                pornPageItem.ImgUrl = imgNode.Attributes["src"].Value.Replace("1_", "");
 
                 string allInfo = listChannel.InnerText;
 
